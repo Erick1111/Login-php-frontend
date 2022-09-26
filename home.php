b287f70fc3e03f1f0247c1349fa32c9bc7f8e997
erick sebastian muñoz castro <sebasboxim@hotmail.com>
2022-09-26 16:09:14 -0500

    Se agrega nuevo commit

---
+++ home.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="css/cabecera.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home</title>
+</head>
+
+<body class="ima2">
+    <link rel="stylesheet" href="estilos.css">
+    <h1> cursos para programar</h1>
+    <a href="https://www.youtube.com/watch?v=Qla90rIkOFc&list=PLg9145ptuAijj9GoHPTcYT8IoQAOchM-n"><br> <br><br>CLICK para visualizar el curso</a>
+    <h2>HTML </h2>
+    <header>
+        <h2>CURRICULUM </h2>
+        <img class="imj">
+    </header>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="estilos.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <h6 style="color:Gray">ingresando a un nuevo mundo</h6>
+</head>
+<body>
+    <form action="validar.php" method="post">
+    <h1 style="color:white;">INICIO DE SESEION</h1>
+    <p class="titulo2">Usuario <input type="text" placeholder="ingrese su nombre" name=" usuario" ></p>
+    <p style="color:Gray">Contraseña <input type="password" placeholder="ingrese su contrasena" name=" contrasena" ></p>
+    <input style="background-color:Gray"  type="submit" value="Ingresar">
+
+    </form>
+</body>
+</html>
